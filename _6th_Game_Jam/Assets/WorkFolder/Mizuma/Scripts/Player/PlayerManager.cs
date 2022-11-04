562bf573db9320c2d8da1b466ebe13ceca8c2264
Mizuma_Nagi <mizumanagi0000@gmail.com>
2022-11-04 17:07:49 +0900

    ダメージ, 敵撃破, アイテム回収のエフェクト導入 / 被ダメージ, アイテム回収のエフェクト表示

--- _6th_Game_Jam/Assets/WorkFolder/Mizuma/Scripts/Player/PlayerManager.cs
+++ _6th_Game_Jam/Assets/WorkFolder/Mizuma/Scripts/Player/PlayerManager.cs
@@ -26,6 +26,8 @@ public class PlayerManager : SingletonClass<PlayerManager>
     public void BirthChild(int num)
     {
         SoundManager.Instance.PlaySE(SEName.Collect_Item);
+        EffectManager.Instance.PlayEffect(EffectManager.EffectType.Collect_Item, new Vector3(transform.position.x, 1.4f, transform.position.z));
+
         playerData.playerLength += num;
         childFactory.BirthChild(num);
         camCon.UpdateCameraView(playerData.playerLength);
@@ -34,6 +36,8 @@ public class PlayerManager : SingletonClass<PlayerManager>
     public void KillChild(int num)
     {
         SoundManager.Instance.PlaySE(SEName.On_Damage);
+        EffectManager.Instance.PlayEffect(EffectManager.EffectType.On_Damage, new Vector3(transform.position.x, 1.4f, transform.position.z));
+
         int afterNum = childFactory.GetCanKillChildCnt(num);
         if (num != afterNum)
         {
