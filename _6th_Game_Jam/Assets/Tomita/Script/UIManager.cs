0c445cbb8d7ecb4db81cd59b62a005b02f97c5ad
RIZE1223 <airhideplus1223@icloud.com>
2022-11-09 17:28:38 +0900

    UIManager

---
+++ _6th_Game_Jam/Assets/Tomita/Script/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnGameStart()
+    {
+
+    }
+    void OnGameEnd()
+    {
+
+    }
+    void UpdateScoreUI(int score)
+    {
+        
+    }
+    void UpdateGameOverUI(float diatance,int maxPlayer,int score )
+    {
+        
+    }
+}
+ 
